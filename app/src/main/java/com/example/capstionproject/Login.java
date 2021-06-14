25d611ea16b00ab738e609dde31e5cf0a734e811
Vinhpham2412 <vinhpxhe130801@fpt.edu.vn>
2021-06-14 11:47:00 +0700

    Create Login,Register Activity

---
+++ app/src/main/java/com/example/capstionproject/Login.java
@@ -0,0 +1,25 @@
+package com.example.capstionproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+    private Button btnRegister;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnRegister=findViewById(R.id.btnLogin2);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/capstionproject/Register.java
+++ app/src/main/java/com/example/capstionproject/Register.java
@@ -3,13 +3,12 @@ package com.example.capstionproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button register;
+public class Register extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
